440346b08ce0735ccf52b544fea9e230ab271f7a
Vignesh Vicky <vigneshvickey2004@gmail.com>
2026-05-14 14:45:18 +0530

    connecingt

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
 app.use(cors({
   origin: "https://shelter-activity.vercel.app/",
