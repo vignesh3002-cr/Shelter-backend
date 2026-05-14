715f1dc651f7f4066a34a5e0ca00765b9cbc90c2
Vignesh Vicky <vigneshvickey2004@gmail.com>
2026-05-14 12:25:33 +0530

    new file is added

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import loginRoute from "../backend/routes/login.js";
-import projectRoute from "../backend/routes/project.js";
+import loginRoute from "./routes/login.js";
+import projectRoute from "./routes/projects.js";
 
 dotenv.config();
 
