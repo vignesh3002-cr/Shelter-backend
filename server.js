070edf74d853a99a9795b8daf461cf5f8b7dbdcc
Vignesh Vicky <vigneshvickey2004@gmail.com>
2026-05-14 12:51:33 +0530

    new file is added

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+  origin: "https://shelter-activity.vercel.app/",
+  credentials: true
+}));
 app.use("/api/auth", loginRoute);
 
 app.use("/api/projects", projectRoute);
