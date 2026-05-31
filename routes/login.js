6fe09dc563a4d483be787ab4a27ce16cf1fdf731
deepakkumar1209 <VIGNESHDEEPAK222@GMAIL.COM>
2026-05-31 18:30:44 +0530

    Refactor login route to simplify response handling

--- routes/login.js
+++ routes/login.js
@@ -1,64 +1,21 @@
 import express from "express";
+import axios from "axios";
 import { getLoginUser } from "../services/d365Services.js";
-
 const router = express.Router();
+
 
 router.post("/login", async (req, res) => {
 
+    
     try {
-
-        const { UserID, password } = req.body;
-
-        const user =
-            await getLoginUser(
-                UserID,
-                password
-            );
-
-        console.log(
-            "Login user from d365:",
-            user
-        );
-
-        res.json({
-
-            success: true,
-
-            userId: UserID,
-
-            userData: user
-        });
-
-    } catch (error) {
-
-        console.log(
-            "Error occurred while logging in:",
-            error
-        );
-
-        if (error.response) {
-
-            return res.status(
-                error.response.status
-            ).json({
-
-                success: false,
-
-                message:
-                    "D365 service unavailable",
-
-                details:
-                    error.response.data
-            });
-        }
-
-        res.status(500).json({
-
-            success: false,
-
-            message:
-                "Internal server error"
-        });
+        const {UserID, password } = req.body;
+        const user = await getLoginUser(UserID, password);
+        console.log("Login user from d365:", user);
+        res.json(user)
+    }
+        catch (error) {
+        console.log("Error occurred while logging in:", error);
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
