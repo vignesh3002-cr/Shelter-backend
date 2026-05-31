093029f7c59d2271d791eacce2e6bfad6a642866
deepakkumar1209 <VIGNESHDEEPAK222@GMAIL.COM>
2026-05-31 18:28:51 +0530

    Refactor my-projects route to remove userId filtering

--- routes/project.js
+++ routes/project.js
@@ -5,38 +5,13 @@ import { verifyToken } from "../middleware/authMiddleware.js";
 import { getProjects } from "../services/d365Services.js";
 
 router.get(
-  "/my-projects/:userId",
-    async (req, res) => {
+  "/my-projects",
+  async (req, res) => {
 
-        try {
+    const projects = await getProjects();
 
-            const { userId } =
-  req.params;
-
-const projects =
-  await getProjects();
-
-const filteredProjects =
-  projects.filter(
-
-    project =>
-
-      project.CreatedBy ===
-      userId
-
-  );
-
-res.json(filteredProjects);
-
-        } catch (error) {
-
-            console.log(error);
-
-            res.status(500).json({
-                message: "Server Error"
-            });
-        }
-    }
+    res.json(projects);
+  }
 );
 
 export default router;
