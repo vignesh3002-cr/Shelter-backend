a30727fe6fd242546bddd0b8302349ad40db8400
Vignesh Vicky <vigneshvickey2004@gmail.com>
2026-06-04 09:06:56 +0530

    new backedn oage of timeview and update routes are added

---
+++ routes/TimeManagement.js
@@ -0,0 +1,35 @@
+import express from "express";
+import {getViewTime} from "../services/d365Services.js";
+import {updateTaskEndDate} from "../services/d365Services.js";
+const router =
+  express.Router();
+  router.post("/ViewTime", async (req, res) => {
+    try {
+
+      const { projectId } =req.body;
+      const timeData =
+        await getViewTime(projectId);
+        console.log(
+          "VIEW TIME DATA:",
+          timeData
+        );
+        res.json(timeData);
+    } catch (error) {
+      console.error("Error fetching view time data:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  router.post("/update-date", async (req, res) => {
+
+    try {
+      const { projectId, wbsId, taskEndDate } = req.body;
+      await updateTaskEndDate(projectId, wbsId, taskEndDate);
+      res.json({ message: "Task end date updated successfully" });
+    } catch (error) {
+      console.error("Error updating task end date:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+
+export default router;
